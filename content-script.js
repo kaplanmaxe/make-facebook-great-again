7e29107e2d82c4f5694311c40896f60149c5c36c
Max Kaplan <mekapl03@syr.edu>
2016-08-11 08:01:14 -0400

    chaging selector and starting to change text

--- content-script.js
+++ content-script.js
@@ -12,29 +12,38 @@ window.onscroll = function() {
 let element_count = 0;
 
 function getNewsFeed() {
-    let user_posts = document.querySelectorAll('[class$=" userContent"]');
+    let user_posts = document.querySelectorAll('[data-testid="fbfeed_story"]');
     console.log(user_posts.length);
     // let user_posts = document.getElementsByClassName("userContentWrapper");
     //If the user loaded more stories
     if (user_posts.length !== element_count) {
-        console.log("HERE");
         //Update element_count to number of posts on page
 
         if (element_count===0) {
             elems = user_posts;
             element_count = user_posts.length;
+            changeStoryText(elems);
             return;
         }
-        else {
-            element_count = user_posts.length;
-        }
+        // else {
+        //     element_count = user_posts.length;
+        // }
+        console.log(element_count + " " + user_posts.length);
         //Loop through new stories and add them to elems
         for (let i=element_count;i<user_posts.length;i++) {
-            elems.push(user_posts[i]);
-            console.log(elems);
+            // elems.push(user_posts[i]);
+            elems[i] = user_posts[i];
+            element_count++;
         }
+        console.log(elems);
+        changeStoryText(elems);
+        return;
     }
-
+}
+function changeStoryText(nodes) {
+  for (let i=0;i<nodes.length;i++) {
+    nodes[i].childNodes[0].innerText = "DONALD TRUMP SUCKS!";
+  }
 }
 // window.setTimeout(function() {
 //
